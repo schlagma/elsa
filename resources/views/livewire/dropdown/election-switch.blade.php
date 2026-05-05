f4c4729017860060e309411200f8d72873db1b90
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-05-06 01:23:09 +0200

    Radio in dropdowns

--- resources/views/livewire/dropdown/election-switch.blade.php
+++ resources/views/livewire/dropdown/election-switch.blade.php
@@ -11,18 +11,20 @@
         @endforeach
     </flux:button>
     <flux:menu>
-        @foreach($elections as $item)
-            <flux:menu.item
-                wire:navigate
-                href="{{ route('election', ['election' => $item->id]) }}"
-                :current="$electionID == $item->id"
-            >
-                @if(app()->getLocale() === 'de')
-                    {{ json_decode($item->name)[0] }}
-                @elseif(app()->getLocale() === 'en')
-                    {{ json_decode($item->name)[1] }}
-                @endif
-            </flux:menu.item>
-        @endforeach
+        <flux:menu.radio.group>
+            @foreach($elections as $item)
+                <flux:menu.radio
+                    wire:navigate
+                    href="{{ route('election', ['election' => $item->id]) }}"
+                    :checked="$electionID == $item->id"
+                >
+                    @if(app()->getLocale() === 'de')
+                        {{ json_decode($item->name)[0] }}
+                    @elseif(app()->getLocale() === 'en')
+                        {{ json_decode($item->name)[1] }}
+                    @endif
+                </flux:menu.radio>
+            @endforeach
+        </flux:menu.radio.group>
     </flux:menu>
 </flux:dropdown>
--- resources/views/livewire/dropdown/language-switch.blade.php
+++ resources/views/livewire/dropdown/language-switch.blade.php
@@ -1,20 +1,29 @@
 <flux:dropdown>
     <flux:button icon:trailing="chevron-down">
-        <x-icon name="flag-language-{{ app()->getLocale() }}" class="size-4" />
-    </flux:button>
-    <flux:menu>
         @foreach(config('app.locales') as $key => $locale)
-            <flux:menu.item
-                wire:navigate
-                href="{{ route('language', ['language' => $key]) }}"
-                class="flex w-full items-center"
-                :current="app()->getLocale() === $key"
-            >
-                <span aria-hidden="true" class="mr-3">
+            @if(app()->getLocale() === $key)
+                <span aria-hidden="true">
                     <x-icon name="flag-language-{{ $key }}" class="size-4" />
                 </span>
-                {{ $locale['name'] }}
-            </flux:menu.item>
+                <span class="sr-only">{{ $locale['name'] }}</span>
+            @endif
         @endforeach
+    </flux:button>
+    <flux:menu>
+        <flux:menu.radio.group>
+            @foreach(config('app.locales') as $key => $locale)
+                <flux:menu.radio
+                    wire:navigate
+                    href="{{ route('language', ['language' => $key]) }}"
+                    class="flex w-full items-center"
+                    :checked="app()->getLocale() === $key"
+                >
+                    <span aria-hidden="true" class="mr-3">
+                        <x-icon name="flag-language-{{ $key }}" class="size-4" />
+                    </span>
+                    {{ $locale['name'] }}
+                </flux:menu.radio>
+            @endforeach
+        </flux:menu.radio.group>
     </flux:menu>
 </flux:dropdown>
